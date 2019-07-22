e2406b3f0f76561a861927f52c8674bdcda34cc5
Marcos Santos <marcossamuel1710@outlook.com>
2019-07-22 13:36:14 -0300

    criado api de cadastro de usuario, configurado os middlewares e o passaport, configurado tmb o .env com a chave do token

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const config = require('../knexfile.js')
 const knex = require('knex')(config)
 
-knex.migrate.latest([config])
+knex.migrate.latest([config]) // migrations de forma automática, se a aplicação for grande, não é interessante fazer isso 
 module.exports = knex
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const app = require('express')()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
 
 app.get('/', (req, res) => {
     res.status(200).send('Iniciado Backend!')
